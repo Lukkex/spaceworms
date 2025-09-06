b04936e7bbd30340321ce65b7f563225cc4cc934
Lukkex <87504872+Lukkex@users.noreply.github.com>
2025-09-05 18:46:53 -0700

    V1

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,34 @@
-use tauri::Manager;
+use mouce::common::{MouseButton, MouseEvent};
+use mouce::Mouse as OtherMouse;
+use mouse_position::mouse_position::Mouse;
+use tauri::{AppHandle, Emitter, Manager};
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+    x: i32,
+    y: i32,
+}
+
+fn listen_for_mouse_events(app_handle: AppHandle) {
+    let mut mouse_manager = OtherMouse::new();
+
+    mouse_manager
+        .hook(Box::new(move |e| match e {
+            MouseEvent::Press(MouseButton::Left) => {
+                let position = Mouse::get_mouse_position();
+                match position {
+                    Mouse::Position { x, y } => {
+                        app_handle
+                            .emit("mouse_click", Payload { x, y })
+                            .expect("Failed to unwrap Mouse position");
+                    }
+                    Mouse::Error => println!("Error getting mouse position"),
+                }
+            }
+            _ => (),
+        }))
+        .expect("Failed to listen for Mouse Events");
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -13,6 +43,8 @@ pub fn run() {
             #[cfg(not(target_os = "linux"))]
             window.maximize().expect("Could not maximize window");
 
+            let app_handle = app.handle().clone();
+            listen_for_mouse_events(app_handle);
             Ok(())
         })
         .run(tauri::generate_context!())
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,91 @@
 import './App.css';
+import { createSignal, onCleanup, createEffect } from 'solid-js';
+import { listen } from '@tauri-apps/api/event';
+
+const images = import.meta.glob('./assets/worms/*.png', { eager: true });
 
 function App() {
-	return <div class="container"></div>;
+	let wormRef: HTMLDivElement;
+
+	type Position = {
+		x: number;
+		y: number;
+	};
+
+	const homePosition = { x: 0, y: 0 };
+
+	const [position, setPosition] = createSignal(homePosition);
+	const [direction, setDirection] = createSignal(0);
+	const [frame, setFrame] = createSignal(1);
+
+	const incrementNumber = () => setFrame((prev) => (prev % 4) + 1);
+
+	createEffect(() => {
+		const interval = setInterval(incrementNumber, 50);
+
+		onCleanup(() => clearInterval(interval));
+	});
+
+	listen<Position>('mouse_click', (event) => {
+		if (sendHome(event.payload)) return;
+
+		calculateDirection(position(), event.payload);
+		setPosition(event.payload);
+	});
+
+	const sendHome = (clickPosition: Position) => {
+		if (!wormRef) return false;
+
+		const boundingBox = wormRef.getBoundingClientRect();
+		if (!boundingBoxIsClicked(boundingBox, clickPosition)) return false;
+
+		calculateDirection(position(), homePosition);
+		setPosition(homePosition);
+		return true;
+	};
+
+	const boundingBoxIsClicked = (boundingBox: DOMRect, clickPosition: Position) => {
+		const { x, y } = clickPosition;
+
+		return (
+			x > boundingBox.left && x < boundingBox.right && y > boundingBox.top && y < boundingBox.bottom
+		);
+	};
+
+	const calculateDirection = (oldPosition: Position, newPosition: Position) => {
+		const x = oldPosition.x - newPosition.x;
+		const y = oldPosition.y - newPosition.y;
+		let angle = (Math.atan2(y, x) * 180) / Math.PI;
+
+		if (angle < 0) angle = 360 + angle;
+
+		const roundToNearest15 = Math.round(angle / 15) * 15;
+		roundToNearest15 === 360 ? 0 : roundToNearest15;
+		setDirection(roundToNearest15);
+	};
+
+	const calculateStyle = (position: Position) => {
+		return `position: absolute; left: ${position.x - 50}px; top: ${position.y - 80}px; 
+    animation-timing-function: ease-in-out;
+    transition: top 1.5s, left 1.5s, transform 0.5s;
+  `;
+	};
+
+	return (
+		<div class="container">
+			<div style={calculateStyle(position())} ref={wormRef}>
+				<img
+					class="worm"
+					width={175}
+					src={
+						images[
+							`./assets/worms/SpaceWormGreen${frame().toString().padStart(1, '0')}.png`
+						].default
+					}
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default App;
